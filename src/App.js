916d8a01e59b7b90b53a884dc4c66e41621847e4
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2022-10-27 15:54:22 +0300

    added BrowserRouter and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useRef, useState } from 'react';
-
+import logo from './logo.svg';
 import './App.css';
 import './ChatList.css';
 import './MessageBox.css';
@@ -11,9 +11,9 @@ import { MessageList } from './components/MessageList';
 import { AUTHORS } from './utils/constants';
 import { Chats } from './components/Chats';
 import { Header } from './components/Header';
+import { BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 
 export const App = () => {
-
 
   const chatList = [
     {
@@ -72,19 +72,52 @@ export const App = () => {
     <div className="App">
       <Header />
       <div className='main'>
-        <div className='message-box'>
-        <div className='chat-list'>
-          <header>Список чатов:</header>
-          <Chats chatList={chatList} />
-        </div>
-          <Form onSubmit={sendMessage}/>
-        </div>
-        <div className='message-list'>
-          <MessageList messageList = {messages} />
-        </div>
-        
+         <BrowserRouter>
+          <div className='main-menu'>
+            <Link to='/'> Главная </Link>
+            <Link to='chats'> Чаты </Link>
+            <Link to='profile'> Профиль </Link>
+          </div> 
+          <div className='app-container'>
+            <Routes>
+              <Route path="/" element={ <StartPage/>} />
+              <Route path="/react" element={ <StartPage/>} />
+              <Route path="chats" element={ 
+                <div className='chat-page'>
+                  <div className='message-box'>
+                    <div className='chat-list'>
+                      <header>Список чатов:</header>
+                      <Chats chatList={chatList} />
+                    </div>
+                    <Form onSubmit={sendMessage}/>
+                  </div>
+                  <div className='message-list'>
+                    <MessageList messageList = {messages} />
+                  </div>  
+                </div>
+              }/>
+              <Route path="profile" element={<UserProfile />}/>
+            </Routes> 
+          </div>
+         </BrowserRouter> 
       </div>
     </div>
 
   );
+}
+
+const StartPage = () => {
+  return (
+    <div className='App-logo'>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+const UserProfile = () => {
+  return (
+    <div>
+      <h3>Профиль пользователя</h3>
+    </div>
+  )
 }
