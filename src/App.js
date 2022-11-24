488c5944fb21a52c05249e1e74c0256d9a58adba
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2022-11-24 13:56:28 +0300

    created pages

--- src/App.js
+++ src/App.js
@@ -1,71 +1,20 @@
 
-import { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
 import './ChatList.css';
 import './MessageBox.css';
+import { ChatPage } from './pages/ChatPage';
+import { StartPage } from './pages/StartPage';
+import { UserProfile } from './pages/UserProfile';
 
-import { Form } from './components/Form';
-import { Id } from './components/GetId';
-import { MessageList } from './components/MessageList';
-import { AUTHORS } from './utils/constants';
-import { Chats } from './components/Chats';
+
 import { Header } from './components/Header';
 import { BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 
 export const App = () => {
 
-  const chatList = [
-    {
-      chatName: 'Макс',
-      chatId: Id(),
-    },
-    {
-      chatName: 'Серега',
-      chatId: Id(),
-    },
-    {
-      chatName: 'Ирина',
-      chatId: Id(),
-    },
-    {
-      chatName: 'Батя',
-      chatId: Id(),
-    }
-  ]
 
-  const [messages, setMessages] = useState([]);
-
-  const timeout = useRef();  // Ссылка на таймаут
-
-  const addMessage = (newMessage) => {
-    setMessages([...messages, newMessage]);
-  }
-
-  const sendMessage = (text) => {
-    addMessage({ 
-      author: AUTHORS.human,
-      text,
-      id: Id(),
-    })
-  };
-
-
-  useEffect(() => {
-      // let timeout;
-      if (messages[messages.length - 1]?.author === AUTHORS.human) {               // ? - optional chaining. Проверяет объект на null, undefined
-        timeout.current = setTimeout(() => {
-          addMessage({ 
-            author: AUTHORS.robot,
-            text: "Your message added!",
-            id: Id(),
-          });
-        }, 1000);
-      }
-      return() => {             // возвращаем каллбэк, который сработает при размонтировании компонента и очистит таймаут
-        clearTimeout(timeout.current);
-      }
-    }, [messages]);
 
 
   return (
@@ -81,21 +30,8 @@ export const App = () => {
           <div className='app-container'>
             <Routes>
               <Route path="/" element={ <StartPage/>} />
-              <Route path="/react" element={ <StartPage/>} />
-              <Route path="chats" element={ 
-                <div className='chat-page'>
-                  <div className='message-box'>
-                    <div className='chat-list'>
-                      <header>Список чатов:</header>
-                      <Chats chatList={chatList} />
-                    </div>
-                    <Form onSubmit={sendMessage}/>
-                  </div>
-                  <div className='message-list'>
-                    <MessageList messageList = {messages} />
-                  </div>  
-                </div>
-              }/>
+              <Route path="/react" />
+              <Route path="chats" element={ <ChatPage/>}/>
               <Route path="profile" element={<UserProfile />}/>
             </Routes> 
           </div>
@@ -106,18 +42,7 @@ export const App = () => {
   );
 }
 
-const StartPage = () => {
-  return (
-    <div className='App-logo'>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  )
-}
 
-const UserProfile = () => {
-  return (
-    <div>
-      <h3>Профиль пользователя</h3>
-    </div>
-  )
-}
+
+
+
